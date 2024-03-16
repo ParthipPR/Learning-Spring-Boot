76a86eb54b11b0ba3a1e09e128f6ceb37a296eb6
ParthipPR <parthip2512003@gmail.com>
2024-03-16 22:24:20 +0530

    --Initial Commit

---
+++ src/test/java/com/parthip/learningsb/LearningsbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parthip.learningsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningsbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
